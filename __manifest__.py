653481e704cf7adc4e3108c994a0431c20a83e67
GemechisGk <gemechis.gk@gmail.com>
2025-06-07 22:54:37 +0300

    Implemented fuel request management feature, including new sequences, access rights, and views. Updated manifest and security files to reflect changes, enhancing overall module functionality.

--- __manifest__.py
+++ __manifest__.py
@@ -9,21 +9,17 @@
         - Employee management
         - Rental agreement management
         - Property management
+        - Fuel request management
     """,
 
     'author': "Infinity Advanced Technology",
     'website': "https://www.inifnity-et.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Services',
     'version': '1.0',
 
-    # any module necessary for this one to work correctly
     'depends': ['base', 'hr', 'mail'],
 
-    # always loaded
     'data': [
         'security/employee_groups_security.xml',
         'security/ir.model.access.csv',
@@ -36,12 +32,14 @@
         'report/rental_agreement_report.xml',
         'report/employee_resume_report.xml',
         'report/property_id_report.xml',
+        'report/fuel_request_report.xml',
         'views/views.xml',
         'views/templates.xml',
         'views/rental_agreement_view.xml',  
         'views/employee_process_view.xml',  
         'views/property_management_view.xml',
         'views/property_ownership_history_view.xml',
+        'views/fuel_request_view.xml',
         'wizard/employee_wizard.xml',
         'wizard/hr_employee_cv_wizard_view.xml',
     ],
